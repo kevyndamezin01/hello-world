ff6e5cb7bc3f81d6df3cc9ebdd3dbb406d6a5b90
kevyndamezin01 <kevyndamezin@gmail.com>
2018-11-16 10:04:51 +0000

    Adding fixes and spelling changes

--- python_bible/rock_paper_scissors.py
+++ python_bible/rock_paper_scissors.py
@@ -12,28 +12,33 @@ welcome_message()
 while True:
 	player_1 = input("Player1 please choose either rock, paper or scissors: ").strip().lower()
 	computer = random.choice(choices)
-	print("So player one has chosen {}".format(player_1))
-	if player_1 == computer():
+	print("Player one has chosen {}".format(player_1))
+	print()
+	print("Good luck player 1")
+	print()
+	print("computer has chosen {}".format(computer))
+	print()
+	if player_1 == computer:
 		print("The game is a draw")
 		break
 
-	elif player_1 == rock and computer == scissors():
+	elif player_1 == "rock" and computer == "scissors":
 		print("Player1 is the winner, well done!!")
 		break
-	elif player_1 == scissors and computer == rock():
+	elif player_1 == "scissors" and computer == "rock":
 		print("The computer is the winner, better luck next time")
 		break
 
-	elif player_1 == scissors and computer == paper():
+	elif player_1 == "scissors" and computer == "paper":
 		print("Player1 is the winner, well done!!")
 		break
-	elif player_1 == paper and computer == scissors():
+	elif player_1 == "paper" and computer == "scissors":
 		print("The computer is the winner, better luck next time")
 		break
 
-	elif player_1 == paper and computer == rock():
+	elif player_1 == "paper" and computer == "rock":
 		print("Player1 is the winner, well done!!")
 		break
-	elif player_1 == rock and computer == paper():
-		print("Unluck the computer is the winner, better luck next time")
-		break	
+	elif player_1 == "rock" and computer == "paper":
+		print("Unlucky the computer is the winner, better luck next time")
+		break
