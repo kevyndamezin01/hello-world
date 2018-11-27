8021aed9e7f007eb1333e31abdcc938adc165885
kevyndamezin01 <kevyndamezin@gmail.com>
2018-11-27 13:05:04 +0000

    Adding changes to temp_sensor.py

--- temp_sensor/temp_sensor.py
+++ temp_sensor/temp_sensor.py
@@ -2,25 +2,45 @@ import serial
 import matplotlib.pyplot as plt
 import time
 
-def open_serial_conection():
-	port = serial.Serial('/dev/tty.usbmodem14201', baudrate=115200)
+port = serial.Serial('COM55', baudrate=115200)
 
-def log_temperature_for_x_seconds(open_serial_conection, time):
+print port.readline()
+print port.readline()
+print port.readline()
+
+def print_id(port):
 	port.flush()
-	port.write("get temp")
-	time.sleep(1)
-	port.readline()
+	port.write("get id\n")
+	print port.readline()
 
-def logged_temperature():	
-	logged_temps = log_temperature_for_x_seconds(open_serial_conection, 5)
+def log_temperature_for_x_seconds(port, seconds):
+	temp = []
+	for s in range(seconds):
+		port.flush()
+		port.write("get temp\n")
+		time.sleep(0.1)
+		temp.append(port.readline())
+	return temp
+	print "finished collecting temperature data"
+
+def print_data(port):
+	port.flush()
+	port.write("get temp\n")
+	print port.readline()
+
+def print_temperature():
+	logged_temps = log_temperature_for_x_seconds(port, 5)
 	temps_int = map(int, logged_temps)
 	print(temps_int)
 
+def plot_temperature():
+	plt.plot([1,2,3,4,5], temps_int)
+	plt.show()
 
 
-open_serial_conection()
-log_temperature_for_x_seconds(op, 5)
-logged_temperature()
+print_id(port)
+log_temperature_for_x_seconds(port, 5)
+print_temperature()
+plot_temperature()
 
-plt.plot([1,2,3,4,5], temps_int)
-plt.show()
+port.close()
