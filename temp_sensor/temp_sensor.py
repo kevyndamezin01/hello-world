701357df357dd1553190e7b1c6a5a1e27ae839da
kevyndamezin01 <kevyndamezin@gmail.com>
2018-11-27 18:25:50 +0000

    Adding changes to temp_sensor.py

--- temp_sensor/temp_sensor.py
+++ temp_sensor/temp_sensor.py
@@ -12,16 +12,30 @@ def print_id(port):
 	port.flush()
 	port.write("get id\n")
 	print port.readline()
+	print port.readline()
 
 def log_temperature_for_x_seconds(port, seconds):
+	print "Collecting temperature readings"
 	temp = []
+	uptime_list = []
+	uptime = 1
 	for s in range(seconds):
 		port.flush()
 		port.write("get temp\n")
 		time.sleep(0.1)
 		temp.append(port.readline())
-	return temp
+		uptime
+		uptime_list.append(uptime)
+		uptime = uptime + 1
+		time.sleep(1)
 	print "finished collecting temperature data"
+	return temp
+	return uptime
+
+def uptime():
+	uptime = 1
+	uptime = uptime + 1
+	time.sleep(1)
 
 def print_data(port):
 	port.flush()
@@ -31,16 +45,16 @@ def print_data(port):
 def print_temperature():
 	logged_temps = log_temperature_for_x_seconds(port, 5)
 	temps_int = map(int, logged_temps)
-	print(temps_int)
+	print temps_int
+	return temps_int
 
 def plot_temperature():
-	plt.plot([1,2,3,4,5], temps_int)
+	plt.plot(uptime_list, print_temperature())
 	plt.show()
 
 
 print_id(port)
-log_temperature_for_x_seconds(port, 5)
+print_data(port)
 print_temperature()
 plot_temperature()
-
 port.close()
