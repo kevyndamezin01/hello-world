141a0d51ea15c949f2e3f7ad84082ff634df8cf5
kevyndamezin01 <kevyndamezin@gmail.com>
2018-11-28 18:33:37 +0000

    updating temp_sensor.py

--- temp_sensor/temp_sensor.py
+++ temp_sensor/temp_sensor.py
@@ -1,6 +1,7 @@
 import serial
 import matplotlib.pyplot as plt
 import time
+import argparse
 
 port = serial.Serial('COM55', baudrate=115200)
 
@@ -16,21 +17,19 @@ def print_id(port):
 
 def log_temperature_for_x_seconds(port, seconds):
 	print "Collecting temperature readings"
-	temp = []
-	uptime_list = []
+	results={'temp': [], 'uptime_list': []}
 	uptime = 1
 	for s in range(seconds):
 		port.flush()
 		port.write("get temp\n")
 		time.sleep(0.1)
-		temp.append(port.readline())
-		uptime
-		uptime_list.append(uptime)
-		uptime = uptime + 1
+		results['temp'].append(port.readline())
+		float(uptime)
+		results['uptime_list'].append(float(uptime))
+		uptime = float(uptime) + 1
 		time.sleep(1)
-	print "finished collecting temperature data"
-	return temp
-	return uptime
+	print "finished collecting temperature readings"
+	return results
 
 def uptime():
 	uptime = 1
@@ -44,14 +43,35 @@ def print_data(port):
 
 def print_temperature():
 	logged_temps = log_temperature_for_x_seconds(port, 5)
-	temps_int = map(int, logged_temps)
+	temps_int = map(int, logged_temps['temp'])
+	uptime_int = logged_temps['uptime_list']
 	print temps_int
-	return temps_int
+	print uptime_int
+	graph = {'temp': temps_int, 'uptime': uptime_int}
+	return graph
 
 def plot_temperature():
-	plt.plot(uptime_list, print_temperature())
+	plt.plot(graph(['uptime']), graph(['temp']))
 	plt.show()
 
+#if __name__ == '__main__':
+#	parser = argparse.ArgumentParser(
+#		description="This script will open a serial connection to an arduino which "\
+#					"will then control a temperature sensor and collect temperature "\
+#					"reading over a given period.")
+#	parser.add_argument("-n", "--num-samples", help="Provide how many temperature readings you would like a second.",
+#					    action='store_true', required=True)
+#	parser.add_argument("-d", "--duration", help="Provide how long you would like to collect temperature readings for.",
+#						action='store_true', required=True)
+#	parser.add_argument("-p", "--port", help="Provide the port which the arduino is conected to.", action='store_true',
+#						required=True)
+#	parser.add_argument("-max", "--max-temp", help="Provide the max temperature that will stop the test if this value is "\
+#	 					"and is read from the sensor", action='store_true', required=False)
+#	parser.add_argument("-min", "--max-temp", help="Provide the max temperature that will stop the test if this value is "\
+#	 					"and is read from the sensor", action='store_tue', required=False)
+#	parser.add_argument("-f", "--filename", help="Provide a filename for the output log", action='store_tue', required=False)
+#	parser.add_argument("-p", "--plot-temps", help="Specifiy wether you want the temperature readings plotted to a graph",
+#						action='store_true', required=Flase)
 
 print_id(port)
 print_data(port)
