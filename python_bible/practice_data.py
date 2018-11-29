8ff930b34d67698321292a04c8f25bf68bcbbf9c
kevyndamezin01 <kevyndamezin@gmail.com>
2018-11-29 21:19:30 +0000

    Adding updates to temp_sensor.py and practice script

--- python_bible/practice_data.py
+++ python_bible/practice_data.py
@@ -1,7 +1,10 @@
 import random
 import time 
 import subprocess
+from logger import Logger
+import sys 
 
+sys.stdout = Loogger(str('Practice_Date.txt'))
 start = time.time()
 
 period_of_time = 120 #2mins
@@ -15,3 +18,23 @@ while True:
 
 	if time.time() > start + period_of_time : break
 	
+
+
+def print_numbers():
+	max_number = 9
+	min_number = 1
+	start = time.time()
+	period_of_time = 60
+	while True:
+		print(random.randint(1,9))
+		time.sleep(1)
+		if print_numbers == max_number : break
+		if print_numbers == min_number : break
+		if time.time() > start + period_of_time : break
+
+while print_numbers() == True:
+	max_number = 9
+	min_number = 1
+	if print_numbers == max_number : break
+	if print_numbers == min_number : break
+
