d2b400004850f2b76c0c1d0cac0d341ea1535311
Kevyn  Damezin <kevyndamezin@Kevyns-MacBook-Pro-2.local>
2019-03-27 22:22:23 +0000

    * Adding changed to hello world c++ code  * Adding a resistor parallel or series network script

--- c++/hello_world.cpp
+++ c++/hello_world.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main()
 {
     int age;
-    cout << "Hello World!`n";
-    cout << "What age are you?`n";
+    cout << "Hello World!\n";
+    cout << "What age are you?\n";
     cin >> age;
-    cout << "You are " << age << " years old.`n";
+    cout << "You are " << age << " years old.\n";
     system("PAUSE");
 
 }
---
+++ c++/resistor_choice.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float(R1);
+    float(R2);
+    float(RT);
+    string(name);
+    string(choice);
+
+    cout << "Hello there what is you're name?\n";
+    cin >> name;
+    cout << "Hello there " << name << "\n";
+
+    cout << "This script will either work out the total resistance of a Parallel Resistor Network or a Series Resistor Network.\n\n";
+
+    cout << "Would you like to work out the total resitance for a Parallel or Series circuit?\n";
+    cin >> choice;
+
+}
